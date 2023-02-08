4dbe213dc54cf7be28a28336c67300fecedca757
npy <42086377+npy0@users.noreply.github.com>
2023-02-08 06:53:33 +0100

    catch exception

--- ghtest-gpu.py
+++ ghtest-gpu.py
@@ -2,7 +2,10 @@ import os
 import nanopy as npy
 
 h = os.urandom(32).hex()
-w = npy.work_generate(h, multiplier=1 / 8)
-print(w)
-print(npy.work_validate(w, h))
-assert npy.work_validate(w, h, multiplier=1 / 8)
+try:
+    w = npy.work_generate(h, multiplier=1 / 8)
+    print(w)
+    print(npy.work_validate(w, h))
+    assert npy.work_validate(w, h, multiplier=1 / 8)
+except RuntimeError as e:
+    print(str(e))
