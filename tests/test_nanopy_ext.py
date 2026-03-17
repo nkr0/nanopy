3cc91f0dc4d897cf0468e030e82b572beb9c1fec
nkr <42086312+nkr0@users.noreply.github.com>
2026-03-17 06:37:52 +0100

    feat: input validation for C functions

--- tests/test_nanopy_ext.py
+++ tests/test_nanopy_ext.py
@@ -1,8 +1,11 @@
 import csv
 from nanopy import ext  # type: ignore
+import pytest
 
 
 def test_work_validate() -> None:
+    with pytest.raises(ValueError, match="Hash must be 32 bytes"):
+        ext.work_validate(0, b"", 0)
     difficulty = int("fffffe0000000000", 16)
     with open("tests/work.csv") as f:
         cf = csv.reader(f)
@@ -12,7 +15,15 @@ def test_work_validate() -> None:
             assert ext.work_validate(work, h, difficulty)
 
 
+def test_work_generate() -> None:
+    with pytest.raises(ValueError, match="Hash must be 32 bytes"):
+        ext.work_generate(b"", 0)
+    ext.work_generate(b"0" * 32, 0)
+
+
 def test_publickey() -> None:
+    with pytest.raises(ValueError, match="Secret key must be 32 bytes"):
+        ext.publickey(b"")
     with open("tests/ed25519.csv") as f:
         cf = csv.reader(f)
         for e in cf:
@@ -22,21 +33,29 @@ def test_publickey() -> None:
 
 
 def test_sign() -> None:
+    with pytest.raises(ValueError, match="Secret key must be 32 bytes"):
+        ext.sign(b"", b"", b"")
+    with pytest.raises(ValueError, match="Random must be 32 bytes"):
+        ext.sign(b"0" * 32, b"", b"")
     with open("tests/ed25519.csv") as f:
         cf = csv.reader(f)
         for e in cf:
             sk = bytes.fromhex(e[0])
-            h = bytes.fromhex(e[2])
+            m = bytes.fromhex(e[2])
             r = bytes.fromhex(e[3])
             sig = bytes.fromhex(e[4])
-            assert sig == ext.sign(sk, h, r)
+            assert sig == ext.sign(sk, m, r)
 
 
 def test_verify_signature() -> None:
+    with pytest.raises(ValueError, match="Signature must be 64 bytes"):
+        ext.verify_signature(b"", b"", b"")
+    with pytest.raises(ValueError, match="Public key must be 32 bytes"):
+        ext.verify_signature(b"0" * 64, b"", b"")
     with open("tests/ed25519.csv") as f:
         cf = csv.reader(f)
         for e in cf:
             pk = bytes.fromhex(e[1])
-            h = bytes.fromhex(e[2])
+            m = bytes.fromhex(e[2])
             sig = bytes.fromhex(e[4])
-            assert ext.verify_signature(sig, pk, h)
+            assert ext.verify_signature(sig, pk, m)
